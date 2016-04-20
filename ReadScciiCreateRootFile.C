03f0a64f5d1feee8f219da037c9eb334a6358684
gsalazarq <gerald.s41@gmail.com>
2016-04-20 15:54:37 -0500

    Create ReadScciiCreateRootFile.C

---
+++ ReadScciiCreateRootFile.C
@@ -0,0 +1,42 @@
+#include "Riostream.h"
+void ReadAsciiCreateRootFile(){
+
+  //  Read data from an ascii file and create a root file with an histogram and an ntuple.
+//   see a variant of this macro in basic2.C
+  //Author: Rene Brun
+
+
+  // read file basic.data
+  //this file has 3 columns of float data
+
+  TString dir = gSystem->UnixPathName(__FILE__);
+  dir.ReplaceAll("ReadAsciiCreateRootFile.C","");
+  dir.ReplaceAll("/./","/");
+  ifstream in;
+  in.open(Form("%sbasic.dat", dir.Data()));
+
+  Float_t x,y,z;
+  Int_t nlines = 0;
+  TFile *f = new TFile("basic.root","RECREATE");
+  TH1F *h1= new TH1F("h1","x distribution", 100, -4,4);
+  TNtuple *ntuple = new TNtuple("ntuple","data from ascii file","x:y:z");
+
+  while(1){
+
+    in >> x >> y >> z; //format of the data
+    if (!in.good()) break;
+    if(nlines<5) printf("x=%8f, y=%8f,z=%8f \n", x,y,z);
+
+    h1->Fill(x);
+    ntuple->Fill(x,y,z  );
+    nlines++;
+
+  }
+
+  printf("found %d points \n", nlines);
+
+  in.close();
+  f->Write();
+
+
+}
