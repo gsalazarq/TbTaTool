2708c00f5c8e3e132493b9e7966c1c5f1580db24
Geraldo <gerald.s41@gmail.com>
2016-04-27 16:35:25 -0500

    NewTool with e39&TBegin feature
    Root file is generate with the comparatio between e39 and TBegin for any type of Run

--- TbTaTool/TbTaTool.C
+++ TbTaTool/TbTaTool.C
@@ -1,12 +1,13 @@
 #define TbTaTool_cxx
 #include "TbTaTool.h"
 #include "TbTaTool_files.h"
+//#include "TbTaTool_files.h"
 //#include "Aux_TbTaTool.h"
 
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
-TString root_path="/home/chasky/Dropbox/min_conocimiento/e45.MINERvA/tools/final_tools/TbTaTool/ThesisVersionTool/";
+
 
 Double_t TbTaTool::NumberEventsInSpill(char* file_name, int value_NumberSpills=0, int r) //fully works
 {
@@ -24,11 +25,9 @@ Double_t TbTaTool::NumberEventsInSpill(char* file_name, int value_NumberSpills=0
 }
 
 
+
 void TbTaTool::NumberEventsInSpillForRun(int file_inicial, int file_end) //fully works
 {
-	//ejemplo file_inicial debe ser el primer valor del file q vamos a trabajar
-	//ejemplo file_end será el q no se tomará en cuenta
-	//seguir el usual
 
 	TFile *f[10000];
 	TTree *tree[10000];
@@ -37,13 +36,151 @@ void TbTaTool::NumberEventsInSpillForRun(int file_inicial, int file_end) //fully
 
 	for(int i=file_inicial; i<file_end; i++)
 	{
-		TbTaToolRun(name_file_electrons[i],i);
-		for(int k=1; k<7; k++)
+		TbTaToolRun(name_file[i],i);
+		for(int k=1; k<value_NumberSpills+1; k++)
 			{
-				cout<< i << "#entradas: "<< NumberEventsInSpill(name_file_electrons[i], 6, k)<< endl;
+				// NumberEventsInSpill(char* file_name, int value_NumberSpills=0, int r)
+				cout<< i << "#entradas: "<< NumberEventsInSpill(name_file[i], value_NumberSpills, k)<< endl;
 			}
 	}
 
+}
+
+
+
+Double_t TbTaTool::BeginTimeOneSubRun(char* file_name ) //fully works
+{
+	Double_t t_begin = -404;
+  Long64_t nbytes = 0, nb = 0;
+
+	 for (Long64_t jentry=0; jentry<1;jentry++) //Loop only the first event
+	 {
+				Long64_t ientry = LoadTree(jentry); if (ientry < 0) break;
+				nb = fChain->GetEntry(jentry);   nbytes += nb; t_begin = (Double_t) Time;
+	 }
+	 t_begin = t_begin-5*3600; //This is good, the inclusion assure us to have the time in Local Time, the time that Leo works
+	 return t_begin;
+}
+
+void TbTaTool::BeginTimeForRun(int i_begin, int i_final) //fullyworks
+{
+
+
+	TString variable1 = "tbegin_spill.txt";
+	char * myfile_name =root_path + type_label + variable1 ;
+	ofstream myfile (myfile_name, ios::out | ios::app);
+	if (myfile.is_open());
+
+	TFile *f[10000];
+	TTree *tree[10000];
+	Double_t t_begin[100000];
+
+
+
+	myfile <<"Unixtime outside of the first event in the spill for " << type_label << " " << variable1 << endl;
+	for(int i=i_begin; i<i_final ; i++) //i number of files
+	{
+		TbTaToolRun(name_file[i],i);
+		cout << i <<"." <<endl;
+		t_begin[i]=BeginTimeOneSubRun(name_file[i]);
+		myfile << t_begin[i]  - 1429810209.67 << endl;
+	}
+ 	myfile << "The End" << endl;
+
+}
+
+Double_t TbTaTool::MatchTwoPoints(int r,  Double_t t_begin)//fully works
+{
+	Double_t value_MatchTwoPoints[2];
+	Double_t delta_time, delta_time_old;
+	int ne39 = 10000; //number of points of e39
+
+	for (int k=0; k<ne39;k++)
+		{
+			 delta_time =  t_begin - e39times[k];
+				 if(delta_time > 0 ){ }
+				 else {
+					 value_MatchTwoPoints[0]=e39times[k-1];
+					 value_MatchTwoPoints[1]=delta_time_old;
+					 break;
+				 }
+				 delta_time_old = delta_time;
+		}
+
+		return value_MatchTwoPoints[r];
+		//value_MatchTwoPoints[0]: e39 time matched_time
+		//value_MatchTwoPoints[1]: delta_time for matched values
+		//value_MatchTwoPoints[2]: tbegin not necessary since BeginTimeOneSubRun() makes the work
+
+
+}
+
+
+
+void TbTaTool::MatchTwoPointsInterval(int i_index=0, int f_index =1, int fenergy=-404, int fpolarity=-404) //only for one
+{
+
+
+
+		//polarityb = -404;energyb=-404;
+
+	for(int i=i_index; i < f_index; i++)			{	//Abrir valor de tbegin
+
+				TbTaToolRun(name_file[i],i);
+				//Get the time of the subrun in local time
+				time_subrun= BeginTimeOneSubRun(name_file[i]);
+				unixtimee39 = MatchTwoPoints(0, time_subrun);
+				delta_time = MatchTwoPoints(1,time_subrun);
+				//cout <<i<<"|";}
+				polarityb = fpolarity;
+				energyb = fpolarity*fenergy;
+				file = i;
+				tree_diff->Fill();
+
+			}
+
+}
+
+void TbTaTool::GenerateMatchedTimesAnalysis(/* arguments */) {
+
+	/* code */
+
+	//Create the root file
+	//Open teh files
+	TString label ="analysis_tbegin_and_e39.root";
+		char * name_results =  root_path +type_label + label;
+		TFile foutput(name_results,"UPDATE");
+	TTree *tree_diff = new TTree("tree_diff","Analysis tbegin and e39");
+
+	Int_t polarityb, energyb, file, number_spill, number_spill_temp;
+	Double_t delta_time, time_subrun, unixtimee39;
+
+	TBranch *b_unixtimee39 = tree_diff->Branch("unixtimee39",&unixtimee39,"unixtimee39/D"); //matched unixtime e39
+		TBranch *b_delta_time = tree_diff->Branch("delta_time", &delta_time, "delta_time/D" ); // interval between the time and the e39
+		TBranch *b_time_subrun = tree_diff->Branch("time_subrun", &time_subrun, "time_subrun/D" ); //tbegin of subrun
+		TBranch *b_energyb = tree_diff->Branch("energyb", &energyb, "energyb/I" );
+		TBranch *b_polarityb = tree_diff->Branch("polarityb", &polarityb, "polarityb/I" );
+		TBranch *b_file = tree_diff->Branch("file", &file, "file/I" );
+
+	for(int k=0; k<super_limit-1; k++)
+	{
+		cout<<"\n="<< k <<"=" << aenergy[k]*apolarity[k] << "GeV=" << endl;
+		polarityb = apolarity[k];
+		energyb = aenergy[k]*apolarity[k];
+		for(int i=aindex[k]; i < aindex[k+1]; i++)			{	//Abrir valor de tbegin
+					TbTaToolRun(name_file[i],i);
+					//Get the time of the subrun in local time
+					time_subrun= BeginTimeOneSubRun(name_file[i]);
+					unixtimee39 = MatchTwoPoints(0, time_subrun);
+					delta_time = MatchTwoPoints(1,time_subrun);
+					file = i;
+					tree_diff->Fill();
+				}
+	}
+
+
+	foutput.Write();
+	cout << "\nend." << endl;
 }
 
 void TbTaTool::Loop()
@@ -59,44 +196,4 @@ void TbTaTool::Loop()
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       // if (Cut(ientry) < 0) continue;
    }
-}
-
-
-Double_t TbTaTool::BeginTimeOneSubRun(char* file_name ) //fully works
-{
-	Double_t t_begin = -404;
-  Long64_t nbytes = 0, nb = 0;
-
-	 for (Long64_t jentry=0; jentry<1;jentry++) //Loop only the first event
-	 {
-				Long64_t ientry = LoadTree(jentry); if (ientry < 0) break;
-				nb = fChain->GetEntry(jentry);   nbytes += nb; t_begin = (Double_t) Time;
-	 }
-
-	 return t_begin;
-}
-
-void TbTaTool::BeginTimeForRun(int i_begin, int i_final) //fullyworks
-{
-	TString variable1 = "tbegin_spill_electrons.txt";
-	char * myfile_name =root_path+ variable1 ;
-	ofstream myfile (myfile_name, ios::out | ios::app);
-	if (myfile.is_open());
-
-	TFile *f[10000];
-	TTree *tree[10000];
-	Double_t t_begin[100000];
-
-	myfile <<"Unixtime outside of the first event in the spill" << endl;
-
-
-	for(int i=i_begin; i<i_final ; i++) //i number of files
-	{
-		TbTaToolRun(name_file_electrons[i],i);
-		cout << i <<"." ;
-		t_begin[i]=BeginTimeOneSubRun(name_file_electrons[i]);
-		myfile << t_begin[i]  - 1435919900 << endl;
-	}
- 	myfile << "End file" << endl;
-
 }
